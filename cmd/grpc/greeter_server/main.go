547a40ebcaa5426175b512295b21ec1e145f1db7
ashin <lqc.ashin@gmail.com>
2019-10-11 10:35:17 +0800

    first commit

---
+++ cmd/grpc/greeter_server/main.go
@@ -0,0 +1,40 @@
+//go:generate protoc -I ../../../pkg/pb/helloworld --go_out=plugins=grpc:../../../pkg/pb/helloworld ../../../pkg/pb/helloworld/helloworld.proto
+
+// Package main implements a server for Greeter service.
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/ashin-l/go-demo/pkg/pb/helloworld"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
